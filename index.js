6bdc19a1837d1fc8eb6b6f02bdabb570083e1f10
thabataganga <ganga.thabata@gmail.com>
2021-12-02 07:17:01 -0300

    exportando modulos

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const express = require('express');
-const app = express();
+const customExpress = require('./config/customExpress');
+
+const app = customExpress();
 
 app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
-
-app.get('/atendimentos', (req, res) => res.send("Você está na rota de atendimentos"));
