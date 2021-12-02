29fe97c2af4ccc5e966d3b2918bc4f9f751174fc
thabataganga <ganga.thabata@gmail.com>
2021-12-02 08:46:14 -0300

    BD conectado

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,10 @@
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send("Você está na rota de atendimentos e esta realizando um GET"));
+    app.get('/atendimentos', (req, res) => {
+        res.send("Você está na rota de atendimentos e esta realizando um GET")
+    });
+
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body);
+        res.send("Voce está na roda de Atendimento e esta realizando um POST")
+    });
 }
--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const customExpress = require('./config/customExpress');
+const conexao = require('./infraestrutura/conexao');
 
-const app = customExpress();
-
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+conexao.connect(erro => {
+    if (erro) {
+        console.log(erro);
+    } else {
+        console.log('BD conectado com sucesso');
+        const app = customExpress();
+        app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+    }
+});
