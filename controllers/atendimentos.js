0a525b5f74d2f0205e5ed56f0d3e8aa016755f95
thabataganga <ganga.thabata@gmail.com>
2021-12-02 09:40:10 -0300

    gravando no bd

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,10 +1,14 @@
+const Atendimento = require('../models/atendimentos');
+
 module.exports = app => {
     app.get('/atendimentos', (req, res) => {
         res.send("Você está na rota de atendimentos e esta realizando um GET")
     });
 
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body);
+        const atendimento = req.body;
+
+        Atendimento.adiciona(atendimento);
         res.send("Voce está na roda de Atendimento e esta realizando um POST")
     });
 }
