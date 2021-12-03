00db6e3cd6b334f42b93c4fac578a255eea63641
thabataganga <ganga.thabata@gmail.com>
2021-12-03 03:34:31 -0300

    patch

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,13 +2,27 @@ const Atendimento = require('../models/atendimentos');
 
 module.exports = app => {
     app.get('/atendimentos', (req, res) => {
-        res.send("Você está na rota de atendimentos e esta realizando um GET")
+        Atendimento.lista(res);
     });
+
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        Atendimento.buscaPorId(id, res);
+        //res.send('OK');
+    })
 
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body;
 
-        Atendimento.adiciona(atendimento);
-        res.send("Voce está na roda de Atendimento e esta realizando um POST")
+        Atendimento.adiciona(atendimento, res);
+        // res.send("Voce está na roda de Atendimento e esta realizando um POST")
     });
+
+    app.patch('/atendimentos/:id', (req, res) => {
+        // converte o ID em número
+        const id = parseInt(req.params.id);
+        const valores = req.body;
+
+        Atendimento.altera(id, valores, res);
+    })
 }
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -2,16 +2,81 @@ const moment = require("moment");
 const conexao = require("../infraestrutura/conexao");
 
 class Atendimento {
-    adiciona(atendimento) {
+    adiciona(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss');
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss');
-        const atendimentoDatado = {...atendimento, dataCriacao, data };
-        const sql = 'INSERT INTO Atendimentos SET ?'
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+        const clienteEhValido = atendimento.cliente.length >= 5;
+
+        const validacoes = [{
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior do que a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
+            }
+        ];
+
+        const erros = validacoes.filter(campo => !campo.valido);
+        const existemErros = erros.length;
+
+        if (existemErros) {
+            res.status(400).json(erros);
+        } else {
+            const atendimentoDatado = {...atendimento, dataCriacao, data };
+
+            const sql = 'INSERT INTO Atendimentos SET ?';
+
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if (erro) {
+                    res.status(400).json(erro);
+                } else {
+                    res.status(201).json(resultados);
+                }
+            })
+        }
+
+
+    }
+
+    lista(res) {
+        const sql = 'SELECT * FROM Atendimentos';
+
+        conexao.query(sql, (erro, resultados) => {
             if (erro) {
-                console.log(erro);
+                res.status(400).json(erro);
             } else {
-                console.log(resultados);
+                res.status(200).json(resultados);
+            }
+        })
+    }
+
+    buscaPorId(id, res) {
+        const sql = `SELECT * FROM Atendimentos WHERE id=${id}`;
+        conexao.query(sql, (erro, resultados) => {
+            const atendimento = resultados[0]
+            if (erro) {
+                res.status(400).json(erro);
+            } else {
+                res.status(200).json(atendimento);
+            }
+        });
+    }
+
+    altera(id, valores, res) {
+        if (valores.data) {
+            valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss');
+        }
+        const sql = 'UPDATE Atendimentos SET ? WHERE id=?';
+        conexao.query(sql, [valores, id], (erro, resultados) => {
+            if (erro) {
+                res.status(400).json(erro);
+            } else {
+                res.status(200).json(resultados);
             }
         })
     }
