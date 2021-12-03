e173ee4de231eb19c0c0dabaa2502751a6397f46
thabataganga <ganga.thabata@gmail.com>
2021-12-03 00:32:10 -0300

    trabalhando com datas

--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -6,13 +6,13 @@ class Tabelas {
     }
 
     criarAtendimentos() {
-        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))';
+        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))';
 
         this.conexao.query(sql, erro => {
             if (erro) {
                 console.log(erro)
             } else {
-                console.log('Tabela de Atendimentos criada com sucesso');
+                console.log('Tabela Atendimentos criada com sucesso')
             }
         });
     }
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,9 +1,13 @@
+const moment = require("moment");
 const conexao = require("../infraestrutura/conexao");
 
 class Atendimento {
     adiciona(atendimento) {
-        const sql = 'INSERT INTO Atendimentos SET ?';
-        conexao.query(sql, atendimento, (erro, resultados) => {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss');
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss');
+        const atendimentoDatado = {...atendimento, dataCriacao, data };
+        const sql = 'INSERT INTO Atendimentos SET ?'
+        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
             if (erro) {
                 console.log(erro);
             } else {
